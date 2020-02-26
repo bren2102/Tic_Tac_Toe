22b9f5e275e6e8b14ff7b6444f3cb9915d78924e
bren2102 <brb.yg.21@gmail.com>
2020-02-26 15:46:08 -0500

    add tie condition and validate user_name

--- lib/logic.rb
+++ lib/logic.rb
@@ -10,6 +10,16 @@ def print_board(array)
   puts "   |   |   "
 end
 
+def validate_user_name(player_name)
+  if player_name.include? " "
+    return false
+  elsif player_name == ''
+    return false
+  else
+    return true
+  end
+end
+
 def validate_input_board_cell(board_array,player_turn,number)
   invalid_input = true
     invalid_input = number < 0 || number > 8
@@ -43,13 +53,15 @@ def win(board_array,player_turn,player_name)
   win_case_3 = cel == board_array[6] && cel == board_array[7] && cel == board_array[8] 
   win_case_4 = cel == board_array[0] && cel == board_array[3] && cel == board_array[6] 
   win_case_5 = cel == board_array[1] && cel == board_array[4] && cel == board_array[7] 
-  win_case_6 = cel == board_array[2] && cel == board_array[3] && cel == board_array[8] 
+  win_case_6 = cel == board_array[2] && cel == board_array[5] && cel == board_array[8] 
   win_case_7 = cel == board_array[0] && cel == board_array[4] && cel == board_array[8] 
   win_case_8 = cel == board_array[6] && cel == board_array[4] && cel == board_array[2]
 
   if win_case_1 || win_case_2 || win_case_3 || win_case_4 || win_case_5 || win_case_6 || win_case_7 || win_case_8
     return "#{player_turn} Wins!"
-  else
+  elsif board_array.all?{ |value| value != ' '}
+    return "Tie"
+  else    
     return ""
   end
 end
