6aad2a4490d6b976579b5abcc5ac499264ae8d9b
bren2102 <brb.yg.21@gmail.com>
2020-02-28 15:22:32 -0500

    add player specs

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,10 +1,26 @@
 require './lib/logic.rb'
+require './lib/player.rb'
 
 describe GameLogic do
   describe "#validate_user_name" do
     it 'validates the user name doesnt have spaces' do
       logic = GameLogic.new
       expect(logic.validate_user_name('Miguel Dubois')).to eql(false)
+    end
+  end
+end
+
+describe Player do
+  describe "#read_name" do
+    it 'returns the name of the player' do
+      player = Player.new('name')
+      expect(player.read_name).to eql('name')
+    end
+  end
+   describe "#read_score" do
+    it 'returns the score of the player' do
+      player = Player.new('name')
+      expect(player.read_score).to eql(0)
     end
   end
 end
